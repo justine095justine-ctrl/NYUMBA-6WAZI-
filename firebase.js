37aef464a098fc8950fc493b595d8976ed60cf56
justine095justine-ctrl <justine095justine@gmail.com>
2026-09-01 22:13:42 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,48 +1,6 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyANpw55jBwxMyf6AC1_y7m6idNYhLP yka0",
-  authDomain: "nyumba-6wazi.firebaseapp.com",
-  databaseURL: "https://nyumba-6wazi-default-rtdb.firebaseio.com",
-  projectId: "nyumba-6wazi",
-  storageBucket: "nyumba-6wazi.appspot.com",
-  messagingSenderId: "401684834308",
-  appId: "1:401684834308:web:a65ab41ee55de814ca2d51"
-};
-firebase.initializeApp(firebaseConfig);
-var db = firebase.database().ref("chat");
-
-// KUKUMBUKA JINA HATA UKIREFRESH
-var jinaLangu = localStorage.getItem("jinaLaChat") || "";
-window.onload = function(){
-  if(jinaLangu != ""){
-    document.getElementById("n").value = jinaLangu;
-    document.getElementById("n").disabled = true;
-  }
-}
-
-db.limitToLast(50).on("child_added",function(s){
-  var d = s.val();
-  if(!d.j || !d.u) return; // Inapuuza ujumbe wa zamani ulioharibika
-
-  var div = document.createElement("div");
-  div.className = "msg " + (d.j == jinaLangu ? "me" : "you");
-  div.innerHTML = "<b>"+d.j+"</b>"+d.u;
-  document.getElementById("chat").appendChild(div);
-  document.getElementById("chat").scrollTop = 99999;
-});
-
-function tuma(){
-  var jinaInput = document.getElementById("n");
-  var ujumbeInput = document.getElementById("m");
-  
-  if(jinaInput.value != "") {
-    jinaLangu = jinaInput.value;
-    localStorage.setItem("jinaLaChat", jinaLangu);
-    jinaInput.disabled = true;
-  }
-  
-  var ujumbe = ujumbeInput.value;
-  if(jinaLangu == "" || ujumbe == "") { alert("Jaza jina na ujumbe"); return; }
-  
-  db.push({j:jinaLangu, u:ujumbe, t:Date.now()});
-  ujumbeInput.value = "";
-}
+const cfg={apiKey:"AIzaSyANpw55jBwxMyf6AC1_y7m6idNYhLP yka0",authDomain:"nyumba-6wazi.firebaseapp.com",databaseURL:"https://nyumba-6wazi-default-rtdb.firebaseio.com",projectId:"nyumba-6wazi",storageBucket:"nyumba-6wazi.appspot.com",messagingSenderId:"401684834308",appId:"1:401684834308:web:a65ab41ee55de814ca2d51"};
+firebase.initializeApp(cfg);
+const db=firebase.database().ref("chat");
+let j=localStorage.j||"";
+onload=()=>{if(j){n.value=j;n.disabled=1}};
+db.limitToLast(50).on("child_added",s=>{let d=s.val();if(!d.j||!d.u)return;let x=document.createElement("div");x.className="msg "+(d.j==j?"me":"you");x.innerHTML="<b>"+d.j+"</b>"+d.u;chat.appendChild(x);chat.scrollTop=99999
